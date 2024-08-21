6be42e888101ef0d89b6e0ed606bc61dcf166011
Gabriel Estes <gabriel.estes@dispatchit.com>
2024-08-21 13:24:56 -0400

    Add address state and basic form

--- app/javascript/components/WeatherIndex.js
+++ app/javascript/components/WeatherIndex.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 const WeatherIndex = (props) => {
+	const [address, setAddress] = useState("");
+	console.log("Address is: ", address);
+
 	return (
 		<React.Fragment>
 			<div>
 				<h1>Weather</h1>
 			</div>
 			<h2>Greeting: {props.greeting}</h2>
+			<form>
+				<input
+					type="text"
+					placeholder="Enter an address"
+					value={address}
+					onChange={(event) => setAddress(event.target.value)}
+				/>
+				<button type="submit">Submit</button>
+			</form>
 			<p>{props.weather}</p>
 		</React.Fragment>
 	);
