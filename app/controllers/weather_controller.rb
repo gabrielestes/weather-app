d9ccf9ea438dacedcea3087e55f9ae038d53dbf9
Gabriel Estes <gabriel.estes@dispatchit.com>
2024-08-21 21:32:42 -0400

    Get caching working

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -5,13 +5,19 @@ class WeatherController < ApplicationController
     @base_url = request.base_url
 
     if weather_params[:lat] && weather_params[:lon]
-      client = TomorrowApi::Client.new(ENV["TOMORROW_API_KEY"])
-      resp = client.forecast(weather_params[:lat], weather_params[:lon])
+      @weather = JSON.parse(retrieve_weather)
+      # @weather = JSON.parse(response.body).to_json
+      render json: @weather
+    end
+  end
 
-      if resp.ok?
-        @weather = JSON.parse(resp.body).to_json
-        render json: @weather
-      end
+  def retrieve_weather
+    cache_key = "weather/#{weather_params[:lat]},#{weather_params[:lon]}"
+
+    Rails.cache.fetch(cache_key) do |_key, options|
+      client = TomorrowApi::Client.new(ENV["TOMORROW_API_KEY"])
+      response = client.forecast(weather_params[:lat], weather_params[:lon])
+      response.body
     end
   end
 
